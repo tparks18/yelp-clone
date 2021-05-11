964a631cc248c73ade33e1fa72cba45cba52580d
Taty <tatyanaparks@gmail.com>
2021-05-11 01:15:51 -0500

    search results list

--- src/Search/SearchResults/SearchResult/SearchResult.js
+++ src/Search/SearchResults/SearchResult/SearchResult.js
@@ -4,9 +4,25 @@ import styles from './SearchResult.module.css';
 export function SearchResult() {
     return (
       <div className={styles[`search-result`]}>
-        <img src='https://via.placeholder.com/150' alt='business image' />
-        <p>General Info</p>
-        <p>Address Data</p>
+        <img
+          src="https://via.placeholder.com/210"
+          alt="business"
+          className={styles["business-image"]}
+        />
+        <div className={styles["business-info"]}>
+          <h2 className="subtitle">Burger Place</h2>
+          <p>Rating</p>
+          <p>
+            $$ <span class="tag">Burgers</span>{" "}
+            <span class="tag">Fast Food</span>
+          </p>
+        </div>
+
+        <div>
+          <p>773-333-3333</p>
+          <p>Example Street</p>
+          <p>1234 Chicago</p>
+        </div>
       </div>
     );
 }
