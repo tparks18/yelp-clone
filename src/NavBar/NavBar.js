90b9364aaee9604ac052638dd3147a536e2e1c53
Taty <tatyanaparks@gmail.com>
2021-05-10 20:36:13 -0500

    navbar edits

---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../assets/yelp-logo-transparent-background-4.png'
+import SearchBar from '../SearchBar/SearchBar';
+import styles from './NavBar.module.css';
+
+export function NavBar() {
+    return (
+      <div className={styles["nav-bar"]}>
+        <img src={logo} className={styles.logo} alt="logo" />
+        <SearchBar small/>
+        <button className={`button ${styles["nav-button"]}`}>Sign In</button>
+        <button className={`button ${styles["nav-button"]}`}>Register</button>
+      </div>
+    );
+}
+
+
--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,36 +1,37 @@
 import React from 'react'
 import styles from './SearchBar.module.css'
 
-function SearchBar() {
+function SearchBar(props) {
+  const sizeClass = props.small ? '' : 'is-medium';
     return (
       <div>
         <div className="field has-addons">
           <p className="control">
-            <button className="button is-static is-medium">
+            <button className={`button is-static ${sizeClass}`}>
               <b>Search</b>
             </button>
           </p>
           <p className="control">
             <input
-              className={`input is-medium ${styles["input-control"]}`}
+              className={`input ${sizeClass} ${styles["input-control"]}`}
               type="text"
               placeholder="plumbers, delivery, takeout..."
             />
           </p>
           <p className="control">
-            <button className="button is-static is-medium">
+            <button className={`button is-static ${sizeClass}`}>
               <b>Near</b>
             </button>
           </p>
           <p className="control">
             <input
-              className={`input is-medium ${styles['input-control']}`}
+              className={`input ${sizeClass} ${styles["input-control"]}`}
               type="text"
               placeholder="Where"
             />
           </p>
-          <div className={`button is-medium ${styles['search-button']}`}>
-            <span className={`icon is-small ${styles['search-icon']}`}>
+          <div className={`button ${sizeClass} ${styles["search-button"]}`}>
+            <span className={`icon is-small ${styles["search-icon"]}`}>
               <i className="fas fa-search"></i>
             </span>
           </div>
